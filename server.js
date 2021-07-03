8cd86465820e5e2f20c4d6a932ebdaa1a3f9e845
ElCapiPrice <micky1996@live.com.mx>
2021-07-03 14:49:54 -0500

    Docs: static web with node

---
+++ server.js
@@ -0,0 +1,13 @@
+//require('dotenv').config();
+/*const http = require('http');
+
+
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello, World!');
+}
+
+const server = http.createServer(requestListener);
+server.listen(8081);
+ */
